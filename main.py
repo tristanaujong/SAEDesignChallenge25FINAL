403bbf874b2b4eb1676a6c809f1e469abc9c76ca
tristanaujong <tristanaujong@gmail.com>
2025-04-17 22:15:24 -0500

    added index.html

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from flask import Flask, request, jsonify, render_template
 import requests # needed to connect to external apis
+from dotenv import load_dotenv
+import os
+
 
 app = Flask(__name__)
 
-base_url = "https://api.mercedes-benz.com/configurator/v2"
+# loads api key
+load_dotenv()
+api_key = os.getenv("MERCEDES_API_KEY")
+
+base_url = "https://api.mercedes-benz.com/configurator/v2/markets/us_US/models"
 
 @app.route("/")
 def index():
